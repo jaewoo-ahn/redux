469e7565794b3ed79f0d8f3e65db10dc135c6f9d
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2022-02-16 18:36:37 +0900

    add : toggle Counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,6 +5,7 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({type: "increment"});
@@ -22,19 +23,21 @@ const Counter = () => {
     dispatch({type: "decrease", amount: 5});
   };
 
-  const zeroHandler = () => {
-    dispatch({type: "zero"});
+  // const zeroHandler = () => {
+  //   dispatch({type: "zero"});
+  // };
+  const toggleCounterHandler = () => {
+    dispatch({type: "toggle"});
   };
-  const toggleCounterHandler = () => {};
   console.log(dispatch);
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>increment</button>
         <button onClick={increment5Handler}>increment 5</button>
-        <button onClick={zeroHandler}>returnZero</button>
+        {/* <button onClick={zeroHandler}>returnZero</button> */}
         <button onClick={decrementHandler}>decrement</button>
         <button onClick={decrement5Handler}>decrement 5</button>
       </div>
